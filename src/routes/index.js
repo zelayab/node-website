6cdfe5f71dd88a406e1ab3e8be8f0d10cfd23f57
Bernardo <Bernardo_zelaya@hotmail.com>
2021-02-26 11:03:36 -0300

    primer commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //routes
-router.get('/index', (req,res) => {
+router.get('/', (req,res) => {
     res.render('index.html',{title: 'First Website'});
 });
 
