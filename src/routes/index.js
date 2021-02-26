b01ab634f715c02625dd20777faaef753361aa28
Bernardo <Bernardo_zelaya@hotmail.com>
2021-02-26 10:54:35 -0300

    make it better

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //routes
-router.get('/', (req,res) => {
+router.get('/index', (req,res) => {
     res.render('index.html',{title: 'First Website'});
 });
 
